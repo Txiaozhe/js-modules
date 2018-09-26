e1212d0b504fb61ed2d04f638eb414864fac1308
TangXiaoji <txiaozhe@gmail.com>
2018-09-26 09:15:26 +0800

    hrtime

--- class/process.js
+++ class/process.js
@@ -5,3 +5,12 @@ console.log(process.argv);
 console.log(process.channel);
 
 console.log(process.env);
+
+const NS_PER_SEC = 1e9;
+const time = process.hrtime();
+
+setTimeout(() => {
+  const diff = process.hrtime(time);
+
+  console.log(`Benchmark took ${diff[0] * NS_PER_SEC + diff[1]} nanoseconds`);
+}, 1000);
