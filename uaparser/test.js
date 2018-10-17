b822b53537f65bd2d0bb5341d57d361d673b2ae4
jim tang <jimtang@tangxiaojideMacBook-Pro.local>
2018-10-17 17:48:42 +0800

    uaparser

--- uaparser/test.js
+++ uaparser/test.js
@@ -4,3 +4,6 @@ const uaparser = require('ua-parser');
 
 const r = uaparser.parse('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
 console.log(r);
+
+const r2 = uaparser.parse('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
+console.log(r);
