337f9cdc50b250aa0c33ca52c40dd6ea3b3f6cc4
jim tang <jimtang@tangxiaojideMacBook-Pro.local>
2018-10-25 11:28:37 +0800

    compose

--- koa/middleware.js
+++ koa/middleware.js
@@ -1,34 +1,35 @@
+'use strict';
+
 const mw = [
-    (ctx, next) => {
-        console.log('fn1 ==> 1');
-        next();
-        console.log('fn1 ==> 2');
+    async (ctx, next) => {
+        console.log('fn1 ==> 1', ctx);
+        await next();
+        console.log('fn1 ==> 2', ctx);
     },
-    (ctx, next) => {
-        console.log('fn2 ==> 1');
-        next();
-        console.log('fn2 ==> 2');
+    async (ctx, next) => {
+        console.log('fn2 ==> 1', ctx);
+        await next();
+        console.log('fn2 ==> 2', ctx);
     },
-    (ctx, next) => {
-        console.log('fn3 ==> 1');
-        next();
-        console.log('fn3 ==> 2');
+    async (ctx, next) => {
+        console.log('fn3 ==> 1', ctx);
+        await next();
+        console.log('fn3 ==> 2', ctx);
     }
 ];
 
 function compose(mw) {
     let index = 0;
-    let fn;
+    let fn = mw[index];
     return function run(context) {
-        fn = mw[index];
-        return Promise.resolve(fn(context, function next() {
+        return Promise.resolve(fn(context, () => { // next
             if (++index < mw.length) {
                 fn = mw[index];
-                run();
+                run(context);
             }
         }));
     }
 }
 
-const res = compose(mw);
-res()
+const run = compose(mw);
+run('ee');
