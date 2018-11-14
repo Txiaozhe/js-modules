5ff40ff9fb2e3ff2fde6c3f69edd4f20c97c5ffe
Txiaozhe <txiaozhe@gmail.com>
2018-11-14 11:18:30 +0800

    test array object

--- class/array.js
+++ class/array.js
@@ -28,9 +28,21 @@
 // console.log(arr.includes(+6));
 
 
-const arr = [];
-arr[8] = 1;
-arr[1] = 3
+// const arr = [];
+// arr[8] = 1;
+// arr[1] = 3
 
-console.log(arr);
-console.log(arr.hasOwnProperty(1));
+// console.log(arr);
+// console.log(arr.hasOwnProperty(1));
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+const c = [].concat(a);
+c.push(7)
+console.log('c: ', c);
+console.log('a: ', a);
+
+const d = a.slice(2);
+console.log('d: ', d);
+console.log('a: ', a);
