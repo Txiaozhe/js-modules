0ba2f09e5d72fb28824a00bc8a60c396cac02a2b
Txiaozhe <txiaozhe@gmail.com>
2018-11-19 19:53:35 +0800

    test

--- esprima/test.js
+++ esprima/test.js
@@ -1,12 +1,22 @@
 const esprima = require('esprima');
 
 const program = `
-const answer = 42;
-function fun() {
-  console.log(answer)
-}
+// const answer = 42;
+// function fun() {
+//   if (answer > 10) {
+//     console.log(answer);
+//   }
+// }
 
-fun();
+// fun();
+
+// if (answer > 10) {
+//   console.log(answer);
+// }
+
+for (var i = 0; i < 10000; i++) {
+  console.log(i);
+}
 `;
 
 const ts_res = esprima.tokenize(program);
