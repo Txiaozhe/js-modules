d4475c5007b4163ae9abdeacb5e32d8b999358b1
Txiaozhe <txiaozhe@gmail.com>
2018-11-28 14:34:59 +0800

    test string

--- class/string.js
+++ class/string.js
@@ -6,3 +6,9 @@ console.log(decoder.write(cent));
 
 const euro = Buffer.from([0xE2, 0x82, 0xAC]);
 console.log(decoder.write(euro));
+
+// make sql
+const s = (['date', 'keyword', 'register'].concat(['']).slice(1).join(' = ?, ')).replace(/\,\s$/, ';');
+console.log(s);
+
+console.log(`${'2018.11.17'.split('.').join('-')} 00:00:00`);
