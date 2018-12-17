4e9f222bc2b60b6086064e8e0b293173805bdb1a
Txiaozhe <txiaozhe@gmail.com>
2018-12-17 14:29:48 +0800

    module.require

--- module/index.js
+++ module/index.js
@@ -1,3 +1,6 @@
+'use strict';
+
+const assert = require('assert');
 const fun1 = require('./test1');
 
 const {test1, test2} = require('./test2');
@@ -8,4 +11,16 @@ console.log(module);
 test1();
 test2();
 
-console.log(require.resolve('./index'));
+describe('test require.resolve', () => {
+  it('os should be a native', () => {
+    assert(require.resolve('os'), 'os');
+  });
+
+  it('bluebird should be a third part', () => {
+    assert(require.resolve('bluebird'), 'bluebird');
+  });
+
+  it('test1 should be a personal', () => {
+    assert(require.resolve('./test1'), '/Users/jimtang/GitHub/js-modules/module/test1.js');
+  });
+});
