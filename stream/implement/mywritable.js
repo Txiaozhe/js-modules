87af33e2f715f3b58c87d51d2c198f3830273071
Txiaozhe <txiaozhe@gmail.com>
2019-01-08 17:49:11 +0800

    Update mywritable.js

--- stream/implement/mywritable.js
+++ stream/implement/mywritable.js
@@ -1,11 +1,21 @@
-const {Writable} = require('stream');
+'use strict';
+
+const { Stream, Writable } = require('stream');
 
 const mywritable = new Writable({
   write(chunk, encoding, cb) {
     console.log('output: ', chunk.toString());
     cb();
-  }
+  },
+  objectMode: true
 });
 
-mywritable.write(null);
+const uint8arr = new Uint8Array();
+
+console.log(uint8arr, uint8arr.toString());
+console.log(Stream._isUint8Array(uint8arr));
+
+mywritable.write({a: 1});
+
+console.log(mywritable);
 process.stdin.pipe(mywritable);
