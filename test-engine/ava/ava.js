3236021f69400f8a294dcb5d68e0d5db59cfe582
Txiaozhe <txiaozhe@gmail.com>
2019-07-01 16:51:38 +0800

    Update ava.js

--- test-engine/ava/ava.js
+++ test-engine/ava/ava.js
@@ -1,5 +1,24 @@
 const test = require('ava');
 
+test.beforeEach(t => {
+    console.log('run===>')
+    t.context = 2;
+})
+
 test(t => {
-    t.deepEqual([1, 2], [1, 2]);
+    console.log('1: ', t.context)
+    t.deepEqual(t.context + 1, 3);
+    console.log('2: ', t.context)
+    t.deepEqual(t.context + 1, 3);
+    console.log('3: ', t.context)
+    t.deepEqual(t.context + 1, 3);
+});
+
+test(t => {
+    console.log('1: ', t.context)
+    t.deepEqual(t.context + 1, 3);
+    console.log('2: ', t.context)
+    t.deepEqual(t.context + 1, 3);
+    console.log('3: ', t.context)
+    t.deepEqual(t.context + 1, 3);
 });
