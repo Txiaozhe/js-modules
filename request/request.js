86f9986243af2d6a26293254690259a696a8a88d
Txiaozhe <txiaozhe@gmail.com>
2019-09-29 12:53:33 +0800

    Update request.js

--- request/request.js
+++ request/request.js
@@ -1,9 +1,29 @@
 
 const request = require('request');
-const demo_pb = require('./demo_pb');
 
-request.get({ url: 'http://txiaozhe.top:3004' }, function(err, httpResponse, body){ 
-    console.log(Buffer.from(body));
-    // const val = demo_pb.DemoMessage.deserializeBinary(Object.values(JSON.parse(body)));
-    // console.log(val.toObject());
-})
+// request.post({
+//     url: '127.0.0.1:8000',
+//     json: {
+//         ddd: 'kkk'
+//     }
+// }, function(err, httpResponse, body){ 
+//     console.log(Buffer.from(body));
+//     // const val = demo_pb.DemoMessage.deserializeBinary(Object.values(JSON.parse(body)));
+//     // console.log(val.toObject());
+// })
+
+function run() {
+    var task = []
+    for(let i = 0; i < 100000; i++) {
+        task.push(new Promise(resolve => {
+            request.get('https://oj.xabc.site/api/announcement', function(err, httpResponse, body){ 
+                resolve(Buffer.from(body).toString())
+            })
+        }))
+    }
+    
+    Promise.all(task)
+}
+
+run()
+
