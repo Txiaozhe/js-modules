abc6a82d61ad73c02206be23829c2bd45dad4d01
Jim Tang <txiaozhe@gmail.com>
2020-03-13 15:03:10 +0800

    Update sort.js

--- class/sort.js
+++ class/sort.js
@@ -1,21 +1,32 @@
-const moment = require('moment');
-
 const arr = [
     {
-        t: '2',
+        t: '1',
         a: '2018-04-05',
-        name: 'ddd'
+        name: 'ddd',
+        id: 4,
+        ts: 1
     },
     {
         t: '3',
         a: '2018-04-05',
-        name: 'ddd'
+        name: 'ddd',
+        id: 4,
+        ts: 3
+    },
+    {
+        t: '3',
+        a: '2018-04-05',
+        name: 'ddd',
+        id: 2,
+        ts: 2
     },
     {
         t: '1',
         a: '2018-04-05',
-        name: 'ddd'
+        name: 'ddd',
+        id: 2,
+        ts: 3
     }
 ]
 
-console.log(arr.sort((a, b) => a.t - b.t));
+console.log(arr.sort((a, b) => a.ts - b.ts || a.id - b.id));
