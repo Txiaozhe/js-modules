73c795ec73fbef928490de4ff44b5f48ef0fb242
Txiaozhe <txiaozhe@gmail.com>
2020-04-16 14:47:05 +0800

    Update 和最小的k对数字.js

--- suanfa/和最小的k对数字.js
+++ suanfa/和最小的k对数字.js
@@ -12,14 +12,15 @@ var kSmallestPairs = function(nums1, nums2, k) {
     return res.sort((_r1, _r2) => _r1.sum - _r2.sum).map(r => r.ele).slice(0, k)
 };
 
+// 双指针法
 var kSmallestPairs2 = function(nums1, nums2, k) {
     let i1 = 0, i2 = 0, result = [ [nums1[i1], nums2[i2]] ]
     for (let i = 0; i < Math.max(nums1.length, nums2.length); i++) {
         i1++
         i2++
-        if (nums1[i1] > nums2[i2]) {
+        if (!nums1[i1] || nums1[i1] > nums2[i2]) {
             i1--
-        } else {
+        } else if (!nums1[i2] || nums1[i1] > nums2[i2]) {
             i2--
         }
         result.push([nums1[i1], nums2[i2]])
@@ -37,5 +38,9 @@ describe('Test', () => {
 
     it('2', () => {
         assert.deepEqual(kSmallestPairs2([1, 7, 11], [2, 4, 6], 3), [[1, 2], [1, 4], [1, 6]])
+    })
+
+    it('3', () => {
+        assert.deepEqual(kSmallestPairs2([1, 7, 11], [2, 4], 2), [[1, 2], [1, 4]])
     })
 })
