4bc9208fddbabf79e98e7ad8b5970bbd7d9af111
Txiaozhe <txiaozhe@gmail.com>
2020-04-19 17:11:40 +0800

    Update 寻找两个有序数组的中位数.js

--- suanfa/寻找两个有序数组的中位数.js
+++ suanfa/寻找两个有序数组的中位数.js
@@ -2,73 +2,11 @@
 
 function solved(nums1, nums2) {
     const len = nums1.length + nums2.length
-    let i = 0, j = 0
     if (len % 2 === 0) {
-        let t1 = len / 2 - 1
-        for (let k = 0; k < len - 1; k++) {
-            if (nums1[k] < nums2[k]) {
-                i++
-                if (nums1[i] === undefined) {
-                    j++
-                    if (j === t1) {
-                        return (nums2[j] + nums2[j - 1]) / 2
-                    }
-                } else {
-                    if (i === t1) {
-                        return (nums1[i] + nums2[j]) / 2
-                    }
-                }
-            } else {
-                j++
-                if (nums2[j] === undefined) {
-                    i++
-                    if (i === t1) {
-                        return (nums1[i] + nums1[i - 1]) / 2
-                    }
-                } else {
-                    if (j === t1) {
-                        return (nums1[i] + nums2[j]) / 2
-                    }
-                }
-            }
-        }
+        const target = len / 2 - 1
+        
     } else {
-        let t1 = Math.floor(len / 2)
-        for (let k = 0; k < len - 1; k++) {
-            if (nums1[k] < nums2[k]) {
-                i++
-                if (nums1[i] === undefined) {
-                    if (i === t1) {
-                        return nums2[j]
-                    } else {
-                        j++
-                        if (j === t1) {
-                            return nums2[j]
-                        }
-                    }
-                } else {
-                    if (i === t1) {
-                        return nums1[i]
-                    }
-                }
-            } else {
-                j++
-                if (nums2[j] === undefined) {
-                    if (j === t1) {
-                        return nums1[i]
-                    } else {
-                        i++
-                        if (i === t1) {
-                            return nums1[i]
-                        }
-                    }
-                } else {
-                    if (j === t1) {
-                        return nums2[j]
-                    }
-                }
-            }
-        }
+        const target = Math.floor(len / 2)
     }
 }
 
